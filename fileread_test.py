7a9ea6aa191c100043df795a8f8d9b5976e6c76f
Hiroki Yuba <yuba3hiroki26@gmail.com>
2018-10-26 20:09:47 +0900

    更新

--- fileread_test.py
+++ fileread_test.py
@@ -6,3 +6,22 @@ Created on Fri Oct 26 16:36:52 2018
 @author: cs22Mac
 """
 
+#import csv
+#
+#csv_file = open("./log_data/Book1.csv", "r", encoding="ASCII", errors="", newline="" )
+##リスト形式
+#f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+#for row in f:
+#    #rowはList
+#    #row[0]で必要な項目を取得することができる
+#    print(row)
+
+import pandas as pd
+
+csv_input = pd.read_csv(filepath_or_buffer="./log_data/Book1.csv", encoding="ASCII", sep=",") # sepはデフォルトで,なので不要
+#値を二次元配列形式?で返却します。
+#返却される型は、numpy.ndarray
+#print(csv_input.values)
+
+#行インデックス、カラムインデックスの順番で指定して項目の値を取得できます。
+print(csv_input.values[:, 1])
