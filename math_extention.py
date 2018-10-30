5b18790e3733522c5f25525cec420e461adc0360
Hiroki Yuba <yuba3hiroki26@gmail.com>
2018-10-31 04:45:57 +0900

    関数名を訂正

--- math_extention.py
+++ math_extention.py
@@ -7,7 +7,7 @@
 import numpy as np
 
 
-def bc2ned(phi, theta, psi, x, y, z):
+def ned2bc(phi, theta, psi, x, y, z):
     '''
     Convert Body Coordinate System to North East Down System.
 
@@ -30,11 +30,6 @@ def bc2ned(phi, theta, psi, x, y, z):
     -------
     ned : array-like
         North East Down System [x, y, z]
-
-    Raises
-    ------
-    ValueError
-        Raises ValueError if size of 'bc' is not 3.
     '''
 
     sin_phi = np.sin(phi)
@@ -49,7 +44,7 @@ def bc2ned(phi, theta, psi, x, y, z):
         [sin_phi*sin_theta*cos_psi-cos_phi*sin_psi, sin_phi*sin_theta*sin_psi+cos_phi*cos_psi, sin_phi*cos_theta],
         [cos_phi*sin_theta*cos_psi+sin_phi*sin_psi, cos_phi*sin_theta*sin_psi-sin_phi*cos_psi, cos_phi*cos_theta]]
     )
-    bc = np.array([x, y, z])
-    ned = np.dot(euler,bc)
+    ned = np.array([x, y, z])
+    bc = np.dot(euler,ned)
 
-    return ned
+    return bc
