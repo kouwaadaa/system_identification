3f52fe6d79e8b73eb32e4c214697022392ad5bf1
YubaHiroki <yuba3hiroki26@gmail.com>
2019-01-04 13:19:08 +0900

    状態方程式を用いて特性を計算する関数ファイルの作成

---
+++ sys_id_freq.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# author: ub
+# 2018/12/14 Fri. 新座標系．
+
+#---------------------------
+# モジュールのインポートなど
+#---------------------------
+
+import const
+import numpy as np
+from numpy import pi
+import pandas as pd
+import math_extention as matex
+
+#---------------------------
+# 周波数特性を調べる
+#---------------------------
+
+def linearlize(format_log_data):
+    '''
+    微小擾乱を考えて線形化された運動方程式を用いて，
+    周波数特性を計算する関数．
+
+    Parameters
+    ----------
+    format_log_data: pandas.DataFrame
+        ピッチ角速度, 迎角, 対気速度, エレベータ舵角, 揚力, 抗力, ピッチモーメント
+        のそれぞれの実験データを含むデータ群．
+
+    Returns
+    -------
+
+    '''
+
+
+    #---------------------------
+    # 整理されたデータから値を取り出す
+    #---------------------------
+
+    data_size = len(format_log_data)
+    d_theta = np.array(format_log_data['d_theta'])
+    alpha = np.array(format_log_data['alpha'])
+    Va = np.array(format_log_data['Va'])
+    delta_e = np.array(format_log_data['delta_e'])
+    L = np.array(format_log_data['L'])
+    D = np.array(format_log_data['D'])
+    Ma = np.array(format_log_data['Ma'])
+    
